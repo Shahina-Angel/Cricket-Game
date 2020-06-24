023d5f5518c7627aaf38ade8526a881d27b26e5c
srinivas <srinivas>
2020-06-24 11:04:24 +0530

    added basic logic to get score

--- script.js
+++ script.js
@@ -1,20 +1,90 @@
-(function (){
-    var runsArr = [0,1,2,3,4,6, "nb", "wb", "out"];
-    var overs = 0;
-    var score = 0;
-    var timer;
+var runsArr = [0, 1, 2, 3, 4, 6, "nb", "wd", "out"];
+var timer;
+var scoreCard = {
+    overs: 0,
+    runs: 0,
+    bowler: "",
+    wickets: 0,
+    econ: 0,
+    run0: [],
+    run4: [],
+    run6: [],
+    wd: 0,
+    nb: 0
+}
+var ballCount = 0;
 
-    function bowl() {
-        console.log("enter")
-        let ind = Math.floor((Math.random() * 9));
-        if (ind !== 6 && ind !== 7 && ind !== 8) {
+function generateRandomNumber() {
+    let num = Math.floor((Math.random() * 9));
+    console.log(num);
+    return num;
+}
 
-        } else {
-            score = score + runsArr[ind];
-            document.getElementById("score").innerText = score;
-        }
+function bowl() {
+    let data = runsArr[generateRandomNumber()];
+
+    switch (data) {
+        case 0:
+            scoreCard.run0++;
+            updateUI("run0", scoreCard.run0);
+            break;
+        case 4:
+            addRuns(data);
+            scoreCard.run4++;
+            updateUI("run4", scoreCard.run4);
+            break;
+        case 6:
+            addRuns(data);
+            scoreCard.run6++;
+            updateUI("run6", scoreCard.run6);
+            break;
+        case "nb":
+            addRuns(1);
+                scoreCard.nb++;
+                updateUI("nb", scoreCard.nb);
+            break;
+        case "wd":
+                addRuns(1);
+                scoreCard.wd++;
+                updateUI("wd", scoreCard.wd);
+            break;
+            case "out":
+                scoreCard.wickets++;
+                updateUI("wickets", scoreCard.wickets);
+            break;
+        default:
+            addRuns(data);
+            break;
     }
 
-    timer = setInterval(bowl, 1000);
+    if (data != "nb" || data != "wd") addBalls();
+}
 
-})();
+function addRuns(val) {
+    scoreCard.runs = scoreCard.runs + val;
+    updateUI("runs", scoreCard.runs);
+}
+
+function updateUI(id, val) {
+    document.getElementById(id).innerText = val;
+}
+
+function addBalls() {
+    ballCount++;
+    let temp = (ballCount && (ballCount % 6 == 0)) ?  ballCount / 6 : (+(scoreCard.overs + 0.1)).toFixed(1);
+    scoreCard.overs = temp;
+    updateUI("overs", scoreCard.overs);
+}
+
+function startBowling() {
+    document.getElementById("btn1").style.display = "none";
+    document.getElementById("btn2").style.display = "block";
+    document.getElementById("scoreCard").style.display = "flex";
+    timer = setInterval(bowl, 1000);
+}
+
+function stopBowling() {
+    clearInterval(timer);
+    document.getElementById("btn1").style.display = "block";
+    document.getElementById("btn2").style.display = "none";
+}
